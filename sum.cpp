d24a37c29c978617e9bd51dd73623232314f93f9
cosimaa <vandoto123@gmail.com>
2023-04-01 16:59:34 +0700

    Add sum.cpp file

---
+++ sum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int sum(int n);
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int a = sum(n);
+    printf("Sum of first N natural numbers is: %d", a);
+    
+}
+
+int sum(int n)
+{
+    if(n != 0)
+    {
+        n = n + sum(n-1);
+    }
+    else 
+    {
+        n = 0;
+    }
+    return n;
+    
+}
