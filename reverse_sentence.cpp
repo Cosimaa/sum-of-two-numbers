b272c5db18c3143205e9c5b7cd9f1a0459ff58bf
cosimaa <vandoto123@gmail.com>
2023-04-03 11:12:58 +0700

    Add reverse_sentence.cpp file

--- reverse_sentence.cpp
+++ reverse_sentence.cpp
@@ -1,29 +1,30 @@
 #include <iostream>
-#include <string.h>
+#include <string>
 using namespace std;
 
-int i = 0 ;
 void reverse(string s);
+long int i = 0;
 
 int main() {
-    string s = "hello" ;
-    reverse(s) ;
+    string s;
+    getline(cin, s);
+    reverse(s);
 }
 
 void reverse(string s) {
-    int len = s.length() ;
-    char temp ;
+    long int len = s.length();
+    char temp;
     temp = s[i] ;
-    s[i] = s[len - 1 - i] ;
-    s[len - 1 - i] = temp ;
-    i++ ;
-    if(i == len) 
+    s[i] = s[len - 1 - i];
+    s[len - 1 - i] = temp;
+    if((len % 2 != 0 && i == len/2) || (len % 2 == 0 && i == len/2 - 1)) 
     {
         s[len] = '\0';
         cout << s;
     }
     else
     {
+        i++;
         reverse(s);
     }
 
